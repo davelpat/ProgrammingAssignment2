92b2b4848d180b908bad676a61c0595c13d6f602
davelpat <davelpat@gmail.com>
2015-02-07 23:39:46 -0800

    fixed makeCacheMatrix to just cache and retrieve, not solve

--- cachematrix.R
+++ cachematrix.R
@@ -4,21 +4,21 @@
 ## this function is a list of functions that store and retrieve
 ## a matrix and its inverse in the parent environment
 
-makeCacheMatrix <- function(cur_matrix = matrix()) {
+makeCacheMatrix <- function(a_matrix = matrix()) {
   # clear out the local inverse so the parent environment is searched for it
   inverse <- NULL
   
   # cache a new matrix and clear out the old inverse from the parent (ie the cache)
-  set <- function(new_matrix) {
-    cur_matrix <<- new_matrix
+  set <- function(a_matrix) {
+    cur_matrix <<- a_matrix
     inverse <<- NULL
   }
   
   # retrieve the current matrix from the cache (parent) environment
   get <- function() cur_matrix
   
-  # create and cache the inverse of the new matrix
-  setinverse <- function(cur_matrix) inverse <<- solve(cur_matrix)
+  # cache the inverse of the new matrix
+  setinverse <- function(a_matrix) inverse <<- a_matrix
   
   # retrieve the current inverse from the global environment
   getinverse <- function() inverse
