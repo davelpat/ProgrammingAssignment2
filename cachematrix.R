fa7a29bdb397efb8710fc65acef9b005d5fdadd6
davelpat <davelpat@gmail.com>
2015-02-08 00:53:04 -0800

    additional code comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this pair of functions creates a singleton inverse for a given matrix
+## and uses the parent environment to cache the matrix and inverse pair
+##
+## makeCacheMatrix is used to create the initial data structure and to
+## cache and retrieve the matrix and its inverse. 
+##
+## cacheSolve generates the inverse of the given matrix
+## or returns the cached inverse if it exists
+##
+## A typical sequence would be:
+##
+## cm <- makeCacheMatrix()  # create the data structure
+## cm$set(some_matrix)      # cache the matrix
+## cacheSolve(cm)           # generate, cache, and return the inverse
+## cacheSolve(cm)           # return the cached inverse
 
-## this function is a list of functions that store and retrieve
-## a matrix and its inverse in the parent environment
+## this function creates a CacheMatrix and the functions that store and
+## retrieve the CacheMatrix and its inverse in the parent environment
 
 makeCacheMatrix <- function(a_matrix = matrix()) {
   # clear out the local inverse so the parent environment is searched for it
@@ -29,8 +42,9 @@ makeCacheMatrix <- function(a_matrix = matrix()) {
        getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## this function returns the cached inverse of the CacheMatrix if it exists
+## or calculates and caches the inverse if it does not exist
+## in either case, it returns the inverse of the CacheMatrix passed to it
 
 cacheSolve <- function(a_matrix, ...) {
   # get the cached inverse, assuming it exists
@@ -43,7 +57,6 @@ cacheSolve <- function(a_matrix, ...) {
     a_matrix$setinverse(inverse)
   } else {
     message("getting cached data")
-    #return(inverse) 
   }
   ## Return a matrix that is the inverse of 'a_matrix'
   inverse
